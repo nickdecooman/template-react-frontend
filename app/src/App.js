4fccf9f0a5a4f3ededa2adf3e1060d837484bd1a
Nick De Cooman <nick@nickdecooman.com>
2017-05-14 12:53:47 +0200

    Get Flow working ⚔️

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,14 @@
+// @flow
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
+const App = (props: { name: string }) => (
   <div className="App">
     <div className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome World!</h2>
+      <h2>Welcome {props.name}!</h2>
     </div>
     <p className="App-intro">
       To get started, edit <code>src/App.js</code> and save to reload.
--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,8 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App name="World" />, document.getElementById('root'));
