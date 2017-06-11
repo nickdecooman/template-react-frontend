cf1b67046b884317189e1b11406c49a21b3205ca
Nick De Cooman <nick@nickdecooman.com>
2017-06-11 22:05:04 +0200

    Aphrodite in ContentBlock component

--- app/src/components/ContentBlock.js
+++ app/src/components/ContentBlock.js
@@ -2,18 +2,34 @@
 
 import React from 'react';
 import { Title, Text } from './Common';
+import { getStyleSheet } from '../utils/aphrodite';
 
 type Props = {
   title: string,
   body: string,
+  styleSheet?: {
+    title: {},
+    body: {},
+  },
 };
 
-const ContentBlock = ({ title, body }: Props) => (
-  <div>
-    <Title>{title}</Title>
-    <hr />
-    <Text>{body}</Text>
-  </div>
-);
+const ContentBlock = ({ title, body, styleSheet }: Props) => {
+  const titleStyleSheet = getStyleSheet('title', styleSheet);
+  const bodyStyleSheet = getStyleSheet('body', styleSheet);
+  return (
+    <div>
+      <Title styleSheet={titleStyleSheet}>{title}</Title>
+      <hr />
+      <Text styleSheet={bodyStyleSheet}>{body}</Text>
+    </div>
+  );
+};
+
+ContentBlock.defaultProps = {
+  styleSheet: {
+    title: null,
+    body: null,
+  },
+};
 
 export default ContentBlock;
--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -2,9 +2,20 @@
 
 import React from 'react';
 import ContentBlock from '../components/ContentBlock';
+import { StyleSheet } from '../utils/aphrodite';
+
+const contentBlockStyles = StyleSheet.create({
+  title: {
+    color: 'blue',
+  },
+});
 
 const About = () => (
-  <ContentBlock title="About" body="This page contains info about me" />
+  <ContentBlock
+    title="About"
+    body="This page contains info about me"
+    styleSheet={contentBlockStyles}
+  />
 );
 
 export default About;
