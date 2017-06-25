c3542e1b13676374cc2290db5ced2985d72b023b
Nick De Cooman <nick@nickdecooman.com>
2017-06-25 10:28:37 +0200

    Add @material/button based Button component

---
+++ app/src/components/Common/Button.js
@@ -0,0 +1,32 @@
+// @flow
+
+import React from 'react';
+import { css } from 'aphrodite/no-important';
+import { type SheetDefinition, getStyle } from '../../utils/aphrodite';
+
+type Props = {
+  label: string,
+  onClick: () => void,
+  styleSheet?: {
+    button?: SheetDefinition,
+  },
+};
+
+const Button = ({ label, onClick, styleSheet }: Props) => {
+  const buttonStyle = getStyle('button', styleSheet);
+  return (
+    <button
+      className={`mdc-button mdc-button--accent ${css(buttonStyle)}`}
+      onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  styleSheet: {
+    button: null,
+  },
+};
+
+export default Button;
--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -2,19 +2,38 @@
 
 import React from 'react';
 import ContentBlock from '../components/ContentBlock';
+import Button from '../components/Common/Button';
 import { StyleSheet } from '../utils/aphrodite';
 
 const contentBlockStyles = StyleSheet.create({
   title: {
     color: 'blue',
   },
+  button: {
+    marginTop: '1em',
+  },
+});
+
+const buttonStyles = StyleSheet.create({
+  button: {
+    marginTop: '1em',
+    background: 'yellow',
+    color: 'black',
+  },
 });
 
 const About = () =>
-  <ContentBlock
-    title="About"
-    body="This page contains info about me"
-    styleSheet={contentBlockStyles}
-  />;
+  <div>
+    <ContentBlock
+      title="About"
+      body="This page contains info about me"
+      styleSheet={contentBlockStyles}
+    />
+    <Button
+      label="Click here"
+      onClick={() => console.log('Hello World')}
+      styleSheet={buttonStyles}
+    />
+  </div>;
 
 export default About;
